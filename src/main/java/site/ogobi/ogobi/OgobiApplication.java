ecee91a532cea03512a28f183b0f796d5ce7126b
kkambbak <yunsung9841@gmail.com>
2023-05-24 15:38:45 +0900

    init

---
+++ src/main/java/site/ogobi/ogobi/OgobiApplication.java
@@ -0,0 +1,13 @@
+package site.ogobi.ogobi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OgobiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OgobiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/ogobi/ogobi/OgobiApplicationTests.java
@@ -0,0 +1,13 @@
+package site.ogobi.ogobi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OgobiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
