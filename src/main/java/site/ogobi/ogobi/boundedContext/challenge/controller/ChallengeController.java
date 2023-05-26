949c653142d2255f94f3f8295a2b5ff6704a8147
kkambbak <yunsung9841@gmail.com>
2023-05-26 17:50:58 +0900

    feat: challenge 초기세팅

---
+++ src/main/java/site/ogobi/ogobi/boundedContext/challenge/controller/ChallengeController.java
@@ -0,0 +1,10 @@
+package site.ogobi.ogobi.boundedContext.challenge.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/challenges")
+public class ChallengeController {
+
+}
---
+++ src/main/java/site/ogobi/ogobi/boundedContext/challenge/repository/ChallengeRepository.java
@@ -0,0 +1,8 @@
+package site.ogobi.ogobi.boundedContext.challenge.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChallengeRepository extends JpaRepository {
+}
