d38d0cbc8a5e93be9b1dd89174551b383205d389
kkambbak <yunsung9841@gmail.com>
2023-05-26 11:55:43 +0900

    feat: 엔티티 초기 세팅

---
+++ src/main/java/site/ogobi/ogobi/boundedContext/challenge/entity/Challenge.java
@@ -0,0 +1,29 @@
+package site.ogobi.ogobi.boundedContext.challenge.entity;
+
+import jakarta.persistence.*;
+import site.ogobi.ogobi.base.baseEntity.BaseEntity;
+import site.ogobi.ogobi.boundedContext.member.Member;
+import site.ogobi.ogobi.boundedContext.spendingHistory.SpendingHistory;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+public class Challenge extends BaseEntity {
+    private String challengeName;
+    private String description;
+
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+
+    private int targetMoney; // 목표 금액
+    private int usedMoney; // 총 사용금액?
+    private int achievementRate; // 달성률
+
+    @ManyToOne(fetch= FetchType.LAZY)
+    private Member member;
+
+    @OneToMany(mappedBy = "challenge", cascade = {CascadeType.ALL})
+    private List<SpendingHistory> spendingHistories;
+
+}
---
+++ src/main/java/site/ogobi/ogobi/boundedContext/member/Member.java
@@ -0,0 +1,24 @@
+package site.ogobi.ogobi.boundedContext.member;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OrderBy;
+import site.ogobi.ogobi.base.baseEntity.BaseEntity;
+import site.ogobi.ogobi.boundedContext.challenge.entity.Challenge;
+
+import java.util.List;
+
+@Entity
+public class Member extends BaseEntity {
+
+    private String nickname; // 닉네임
+
+    private String username; // 진짜 로그인할때 쓰는 아이디
+    private String password;
+
+    @OneToMany(mappedBy = "member", cascade = {CascadeType.ALL})
+    @OrderBy("id desc")
+    private List<Challenge> challenge;
+
+}
