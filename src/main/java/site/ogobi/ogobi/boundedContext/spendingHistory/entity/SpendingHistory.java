f45f127efbf6d47b910349dc3789cf2486041aed
Minsoo Choi <lucas0209@yonsei.ac.kr>
2023-05-30 16:19:07 +0900

    fix: update 메서드 수정 내역 수정

--- src/main/java/site/ogobi/ogobi/boundedContext/spendingHistory/entity/SpendingHistory.java
+++ src/main/java/site/ogobi/ogobi/boundedContext/spendingHistory/entity/SpendingHistory.java
@@ -6,14 +6,18 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 import site.ogobi.ogobi.base.baseEntity.BaseEntity;
 import site.ogobi.ogobi.boundedContext.challenge.entity.Challenge;
+//import site.ogobi.ogobi.boundedContext.file.UploadFile;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Getter
+@Setter
 @SuperBuilder
 @NoArgsConstructor
 public class SpendingHistory extends BaseEntity {
@@ -22,13 +26,16 @@ public class SpendingHistory extends BaseEntity {
     private int price; //각 사용금액 ex)4500원
     private LocalDateTime date;
     private String description;
+//    private UploadFile imageFile;
 
     private String attachmentPath;
 
     @ManyToOne(fetch= FetchType.LAZY)
     private Challenge challenge;
 
-    public void updateDescription(String description) {
+    public void update(String content, int price, String description) {
+        this.content = content;
+        this.price = price;
         this.description = description;
     }
 }
