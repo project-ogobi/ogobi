aa318643f4ce5b3732cdea59f99aadfd50ac4106
eoeo2255 <naiga6@gmail.com>
2023-05-30 17:34:16 +0900

    design: 좋아요 버튼 생성

--- src/main/java/site/ogobi/ogobi/boundedContext/post/entity/Post.java
+++ src/main/java/site/ogobi/ogobi/boundedContext/post/entity/Post.java
@@ -2,7 +2,6 @@ package site.ogobi.ogobi.boundedContext.post.entity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
@@ -11,26 +10,22 @@ import site.ogobi.ogobi.boundedContext.comment.entity.Comment;
 import site.ogobi.ogobi.boundedContext.member.entity.Member;
 
 import java.util.List;
+import java.util.Set;
 
 @Getter
 @Entity
 @SuperBuilder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post extends BaseEntity {
-
-    private Category category;
     @Column(length = 20)
     private String subject;
     @Column(columnDefinition = "TEXT")
     private String content;
     @ManyToOne
     private Member author;  //  작성한 멤버
-
     @OneToMany(mappedBy = "post", cascade = {CascadeType.ALL})
     private List<Comment> comments;
+    @ManyToMany
+    private Set<Member> like;
 
-    public enum Category {
-        FREE,
-        SHARING
-    }
 }
