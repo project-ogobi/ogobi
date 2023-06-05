aea60351fbdb22c7bc4c65483d41fc422c11d2b9
eoeo2255 <naiga6@gmail.com>
2023-06-05 23:25:27 +0900

    feat: 로직 작성, 선준비

--- src/main/java/site/ogobi/ogobi/boundedContext/member/controller/MemberController.java
+++ src/main/java/site/ogobi/ogobi/boundedContext/member/controller/MemberController.java
@@ -7,9 +7,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import site.ogobi.ogobi.base.rq.Rq;
+import site.ogobi.ogobi.boundedContext.challenge.entity.Challenge;
 import site.ogobi.ogobi.boundedContext.challenge.service.ChallengeService;
 import site.ogobi.ogobi.boundedContext.member.entity.Member;
 import site.ogobi.ogobi.boundedContext.member.service.MemberService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -26,6 +30,26 @@ public class MemberController {
         Member member = rq.getMember();
 
         model.addAttribute("member", member);
+        return "mypage/home";
+    }
+
+    @GetMapping("/closed")
+    public String closedChallenge(Model model){
+        Member member = rq.getMember();
+        List<Challenge> challengeList = challengeService.findAll();
+        List<Challenge> successList = new ArrayList<>();
+
+        for (Challenge ch:challengeList) {
+            if (ch.isSuccess()){
+                successList.add(ch);
+            }
+        }
+
+        if (successList.size()==0){
+            return rq.historyBack("완료된 챌린지가 없습니다.");
+        }
+
+        model.addAttribute(successList);
         return "mypage/home";
     }
 
