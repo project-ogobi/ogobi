f4d794523dee0e5fbda492e187ffc9ae26f300bb
yeonann <yyeonann@gmail.com>
2023-06-12 22:40:23 +0900

    feat: api 호출로 서울시 착한가게 목록 가져오기

---
+++ src/main/java/site/ogobi/ogobi/boundedContext/goodPlace/StoreData.java
@@ -0,0 +1,28 @@
+package site.ogobi.ogobi.boundedContext.goodPlace;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StoreData {
+    @JsonProperty("SH_ID")
+    private Long id;
+    @JsonProperty("SH_NAME")
+    private String name; // 가게 이름
+    @JsonProperty("INDUTY_CODE_SE")
+    private int categoryCode; // 분류 코드
+    @JsonProperty("INDUTY_CODE_SE_NAME")
+    private String categoryName;
+    @JsonProperty("SH_ADDR")
+    private String address;
+    @JsonProperty("SH_PHONE")
+    private String phoneNumber;
+    @JsonProperty("SH_PHOTO")
+    private String photo;
+}
