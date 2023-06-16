089b07ac3e9d4961587be9f02228173e59c5015f
kkambbak <yunsung9841@gmail.com>
2023-06-16 10:39:44 +0900

    fix: 공유게시판에서만 에러 표시

--- src/main/java/site/ogobi/ogobi/boundedContext/post/controller/PostController.java
+++ src/main/java/site/ogobi/ogobi/boundedContext/post/controller/PostController.java
@@ -145,7 +145,7 @@ public class PostController {
         Post.Category postCategory = getCategory(category);
         Member member = this.memberService.getMember(principal.getName());
 
-        if(postDto.getChallengeId() == null){
+        if(postDto.getChallengeId() == null && category.equals("sharing")){
             return rq.historyBack("챌린지를 선택해주세요.");
         }
 
